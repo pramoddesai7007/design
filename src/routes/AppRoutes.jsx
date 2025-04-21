f3da2efb5ffaebd4495ef37162f2d39ecb2adc7b
KD3_70202_PramodDesai <pramoddesai7007@gmail.com>
2025-04-21 11:33:44 +0530

    Fix: corrected Home component path issue

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home/Home';
+import Home from '../pages/home/Home';
 import Blog from '../pages/blog/Blog';
 import About from '../pages/about/About';
 import Contact from '../pages/contact/Contact';
